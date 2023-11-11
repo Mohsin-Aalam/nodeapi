d81a41883f311bc02082db5471e4a8a432bd078c
Mohsin Alam <144471554+Mohsin-Aalam@users.noreply.github.com>
2023-11-11 17:41:28 +0530

    updatw

--- src/server.js
+++ src/server.js
@@ -1,12 +1,11 @@
 //initialization 
-const express =require('express');
-const app = express(); 
+const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
-const note =require('./models/note_modal');
+const note = require('./models/note_modal');
 
-const bodyParser=require('body-parser');
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 //true ->nested objects (correct)
 //false -> nested objects (not correct)
 
@@ -14,47 +13,52 @@ app.use(bodyParser.json());
 
 
 mongoose.connect('mongodb+srv://greenstore987:greenstore@cluster0.hdkvf8p.mongodb.net/notesdb')
-.then(function(){
-//home route (/)
-app.get("/", function(req,res){
-     const response = {message:"API WORKS!"};
-     res.json(response);  
-});
-app.post("/notes/list", async function(req,res){
-    var notes=await note.find({userid:req.body.userid});
-    res.json(notes);
-    
-});
-app.post("/notes/add", async function(req,res){
-    
-       await note.deleteOne({id:req.body.id});
-   
-    const newNote=new note({
-        id:req.body.id,
-        userid:req.body.userid,
-        title:req.body.title,
-        content:req.body.content
+    .then(function () {
+
+        console.log("db connected..")
     });
-   await newNote.save();
 
-   const response={message:"new note created!"+ `id: ${req.body.id}`};
+app.get("/", function (req, res) {
+    const response = { message: "API WORKS!" };
     res.json(response);
+});
+
+app.get("/notes/list", async function (req, res) {
+    var notes = await note.find({ userid: req.query.userid });
+    res.status(200).json({data: notes});
+});
+
+app.post("/notes/add", async function (req, res) {
     
+    var isNote = await note.findOne({ userid: req.body.userid, id: req.body.id });
+    if(isNote){
+       return res.status(403).json({
+        message: "already exist"
+       })
+    }
+
+
+
+    const newNote = new note({
+        id: req.body.id,
+        userid: req.body.userid,
+        title: req.body.title,
+        content: req.body.content
+    });
+    await newNote.save();
+
+    const response = { message: "new note created!" + `id: ${req.body.id}` };
+    res.status(201).json(response);
 });
 
-app.post("/notes/delete",async function(req,res){
-   await note.deleteOne({id:req.body.id});
-   const response={ message:"note deleted !"+`id:${req.body.id}`};
-   res.json(response);
-});
-
-
-
-    console.log("db connected..")
+app.delete("/notes/delete", async function (req, res) {
+    await note.deleteOne({ id: req.body.id });
+    const response = { message: "note deleted !" + `id:${req.body.id}` };
+    res.json(response);
 });
 
 
 //starting the server on the port 
-app.listen(5000,function(){
+app.listen(5000, function () {
     console.log("server started at port 5000");
 });
